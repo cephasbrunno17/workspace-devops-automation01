3e307903cbbef212aae2521b73b5e47616e810ba
cephasbrunno17 <brunnoramalho2016@gmail.com>
2026-08-17 21:55:03 -0300

    ajuste nos dados do terraform

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,18 +1,18 @@
 variable "resource_group_name" {
   description = "Nome do Resource Group"
   type        = string
-  default     = "rg-vm-automation"
+  default     = "rg-tfstate"
 }
 
 variable "location" {
   description = "Região do Azure"
   type        = string
-  default     = "East US"
+  default     = "Central US"
 }
 
 variable "admin_password" {
   description = "Senha do usuário administrador"
   type        = string
   sensitive   = true
-  default     = "Torresmo!@123!(*@)"
+  default     = "Labs123@"
 }
